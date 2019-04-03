027466acd8ba8f2332af79468ae0e818c7756932
ronn <ronnie-filth@hotmail.com>
2019-04-03 22:40:52 +0200

    Add Payment

---
+++ src/main/java/com/intrum/refinance/RefinanceController.java
@@ -0,0 +1,11 @@
+package com.intrum.refinance;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RefinanceController {
+
+
+}
