8725302e093ae27fdfe0b1c363b0ac9ccf9088a1
ronn <ronnie-filth@hotmail.com>
2019-04-03 20:10:43 +0200

    First commit

---
+++ src/main/java/com/intrum/refinance/RefinanceApplication.java
@@ -0,0 +1,13 @@
+package com.intrum.refinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intrum/refinance/RefinanceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.intrum.refinance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RefinanceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
